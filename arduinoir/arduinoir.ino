9b6338a62d6250d4661c3f0064d0af918622413b
kerr <hey@gmail.com>
2016-12-10 22:09:00 +0800

    12_10

--- arduinoir/arduinoir.ino
+++ arduinoir/arduinoir.ino
@@ -1,10 +1,14 @@
 int Led=13;
+#define FALSE 0
+#define TRUE 1
 int buttonpin=3;
 int val;
+int i=0,j=0;
 static int g_counter = 0;
+//http://www.yd-tech.com.tw/product_info.php/products_id/70965
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200); 
+  Serial.begin(9600); 
   pinMode(Led,OUTPUT);
   pinMode(buttonpin,INPUT);
 }
@@ -14,11 +18,14 @@ void loop() {
   val=digitalRead(buttonpin);
   if(val==HIGH){
     digitalWrite(Led,HIGH);
-    //Serial.println(g_counter);
-    //g_counter++;
-    Serial.print("ok");
+   
     }else{
       digitalWrite(Led,LOW);
-      //Serial.print("no");
+      i=1;
+      delay(1000);
+      if(i){
+        j++;
+        Serial.print(j);
+        }
       }
 }
--- arduinow5100/arduinow5100.ino
+++ arduinow5100/arduinow5100.ino
@@ -3,7 +3,7 @@
 byte mac[]={0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02};//指定網路卡 MAC 位址 (隨便設)
 EthernetClient client;// 建立乙太網物件 EthernetClient
 void setup() {//設定初始值
-  Serial.begin(115200);//初始化序列埠
+  Serial.begin(9600);//初始化序列埠
   //啟用 Ethernet 連線，預設會以 DHCP 取得 IP 位址
   if (Ethernet.begin(mac)==0) {
     Serial.println("Cannot get IP Address!");   
