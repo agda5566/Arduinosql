1607e82917b91455168eb29abba99c0c301fcbf5
kerr <hey@gmail.com>
2017-01-16 22:04:21 +0800

    ok all1_16

--- arduinotrytest/arduinotrytest.ino
+++ arduinotrytest/arduinotrytest.ino
@@ -1,10 +1,18 @@
+#include <dht.h> //DHT函式庫
+#define dht_dpin 2 //溫度資訊從pin2進來
+#include <HX711.h>
+
+HX711 hx(9, 10,128,0.00127551);//秤重
+dht DHT;//溫度
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  
+  hx.set_offset(41562);
 }
 
 void loop() {
+  DHT.read11(dht_dpin);
   // put your main code here, to run repeatedly:
   if(Serial.available()>0)
   {
@@ -12,10 +20,10 @@ void loop() {
      switch(a)
      {
       case '1':
-        sql();
+        kg();
         break;
       case '2':
-        rfid();
+       rfid();
         break;
       case '3':
         tch();
@@ -27,9 +35,24 @@ void loop() {
   }
 }
 
-void sql()
+void kg()
 {
-  Serial.println("1sql");
+  double sum0=0;
+  double sum1=0;
+  double a=0;
+  for(int i=0;i<10;i++){
+    sum0+=hx.read();
+    sum1+=hx.bias_read();
+  }
+  a=(sum1/10)-51;
+  //Serial.println("ks:"); 
+  if(a<1){
+    Serial.println("0");
+  }
+  else{
+    Serial.println(a);
+  }
+  //delay(500);
 }
 void rfid()
 {
@@ -37,5 +60,14 @@ void rfid()
 }
 void tch()
 {
-  Serial.println("tch");  
+  
+  //Serial.println("tch");
+  //double humidity=DHT.humidity;
+  //Serial.print(humidity);
+  //Serial.print("% ");
+  //Serial.print("temperature = ");
+  double temperature=DHT.temperature;
+  Serial.println(temperature);
+  //Serial.print(DHT.temperature);
+  //Serial.println("C ");  
 }
